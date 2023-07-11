f8487ab1d81d15759a9836c433e670c6941771ee
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-07-10 22:43:28 -0500

    Moved pseudo coding around to match to the right section

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,11 @@ $(function () {
     var displayHr = $(this).attr('id').split('-')[1];
 
     // TODO: Add code to get any user input that was saved in localStorage and set
+    // block by comparing the id to the current hour. HINTS: How can the id
+    // attribute of each time-block be used to conditionally add or remove the
+    // past, present, and future classes? How can Day.js be used to get the
+    // current hour in 24-hour time?
+    //
     var txtVal = localStorage.getItem(displayHr);
     var displayTxt = $(this).find('.description');
     displayTxt.val(txtVal);
@@ -42,11 +47,6 @@ $(function () {
       $(this).find('.description').addClass('future');
     }
   })
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
 
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
